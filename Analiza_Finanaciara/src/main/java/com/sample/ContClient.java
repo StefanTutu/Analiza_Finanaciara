3cc55d58ee91f32e6512147389aecf8ad297ef31
Stefan Tutu <tutustefanvalentin@gmail.com>
2017-12-17 23:54:55 +0200

    Fixed fixed merge :D

--- Analiza_Finanaciara/src/main/java/com/sample/ContClient.java
+++ Analiza_Finanaciara/src/main/java/com/sample/ContClient.java
@@ -1,73 +1,9 @@
-<<<<<<< HEAD:Analiza_Finanaciara/src/main/java/com/sample/ContClient.java
 package com.sample;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class ContClient {
-
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="id")
-	private Long id;
-	
-	@Column(name="denumire")
-	private String denumire;
-	
-	@Column(name="suma")
-	private Double suma;
-	
-	@Enumerated(EnumType.STRING)
-	@Column(name="Tip_Cont")
-	TipCont TipCont;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDenumire() {
-		return denumire;
-	}
-
-	public void setDenumire(String denumire) {
-		this.denumire = denumire;
-	}
-
-	public Double getSuma() {
-		return suma;
-	}
-
-	public void setSuma(Double suma) {
-		this.suma = suma;
-	}
-
-	public ContClient(Long id, String denumire, Double suma) {
-		super();
-		this.id = id;
-		this.denumire = denumire;
-		this.suma = suma;
-	}
-
-	public ContClient() {
-		super();
-	}
-
-}
-=======
-package com.sample;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -90,6 +26,11 @@ public class ContClient {
 	@Column(name="suma")
 	private Double suma;
 
+	/*	sau aici mai ziceam daca e sa punem tipul asta :-??
+	 * @Enumerated(EnumType.STRING)
+	@Column(name="Tip_Cont")
+	TipCont TipCont;*/
+	
 	public long getId() {
 		return id;
 	}
@@ -126,4 +67,3 @@ public class ContClient {
 	}
 
 }
->>>>>>> andreeaBranch:src/main/java/com/sample/ContClient.java
